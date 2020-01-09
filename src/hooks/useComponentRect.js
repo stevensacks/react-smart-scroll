aa80a38b931049251c920a7a7b1a5f44f278c53e
Steven Sacks <stevensacks@gmail.com>
2020-01-09 17:49:14 +0900

    fixes #3 check for ref before accessing current

--- src/hooks/useComponentRect.js
+++ src/hooks/useComponentRect.js
@@ -3,15 +3,17 @@ import {useEffect, useState} from 'react';
 export default ref => {
     const [rect, setRect] = useState({});
     useEffect(() => {
-        const {current} = ref;
-        const onResize = () => {
-            setRect(current.getBoundingClientRect());
-        };
-        if (current) {
-            window.addEventListener('resize', onResize);
-            setRect(current.getBoundingClientRect());
+        if (ref) {
+            const {current} = ref;
+            const onResize = () => {
+                setRect(current.getBoundingClientRect());
+            };
+            if (current) {
+                window.addEventListener('resize', onResize);
+                setRect(current.getBoundingClientRect());
+            }
+            return () => window.removeEventListener('resize', onResize);
         }
-        return () => window.removeEventListener('resize', onResize);
     }, [ref]);
     return rect;
 };
